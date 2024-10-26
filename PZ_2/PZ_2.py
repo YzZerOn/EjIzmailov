8e96c063ab65fd0efe8702b6179d892ecdb0d939
YzZerOn <enjoyraxining@gmail.com>
2024-10-26 17:25:20 +0300

    Исправил код в ПЗ №2

--- PZ_2/PZ_2.py
+++ PZ_2/PZ_2.py
@@ -8,10 +8,10 @@ def intcheck(x):
             print('Неправильный ввод числа!')
             x = input('Повторите попытку: ')
 a = input('Введите число A: ')
-a = floatcheck(a)
+a = intcheck(a)
 b = input('Введите число B: ')
-b = floatcheck(b)
+b = intcheck(b)
 if a>b and a>0 and b>0:
-    print(int(a % b))
+    print(a % b)
 else:
     print('A не должно быть меньше чем B или числа должны быть положительными')
