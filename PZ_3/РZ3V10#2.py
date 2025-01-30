c13f76db28d6d6c732cc1cb86e07e85b2fb472c3
YzZer1 <enjoyraxining@gmail.com>
2025-01-30 23:44:14 +0300

    Update РZ3V10#2.py

--- PZ_3/РZ3V10#2.py
+++ PZ_3/РZ3V10#2.py
@@ -1,7 +1,22 @@
-mes = int(input("Введите номер месяца "))
-if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+"""Вариант 10.
+Дано целое число N (> 1). Найти наибольшее целое число K, при котором
+выполняется неравенство 3^K < N."""
+def intcheck(x):
+    while type(x) != int:
+        try:
+            x = int(x)
+            if 1>x or x>12:
+                raise ValueError
+            return x
+        except ValueError:
+            print('Неправильный ввод месяца!')
+            x = input('Повторите попытку: ')
+
+mes = intcheck(input("Введите номер месяца: "))
+
+if mes in (1, 3, 5, 7, 8, 10, 12):
     print("Дней в месяце - 31")
-elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+elif mes in (4, 6, 9, 11):
     print("Дней в месяце - 30")
 elif mes == 2:
     print("Дней в месяце - 28")
