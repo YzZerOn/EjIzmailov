92493350c05e476b535853c601978e5548592e1a
YzZer1 <enjoyraxining@gmail.com>
2025-01-30 23:44:40 +0300

    Update PZ4V10#2.py

--- PZ_4/PZ4V10#2.py
+++ PZ_4/PZ4V10#2.py
@@ -1,22 +1,20 @@
 """Вариант 10.
 Дано целое число N (> 1). Найти наибольшее целое число K, при котором
 выполняется неравенство 3^K < N."""
-def intcheck(x):
-    while type(x) != int:
-        try:
-            x = int(x)
-            if 1>x or x>12:
-                raise ValueError
-            return x
-        except ValueError:
-            print('Неправильный ввод месяца!')
-            x = input('Повторите попытку: ')
+while True:
+    try:
+        n = int(input("Введите целое число N (> 1): "))
+        if n > 1:break
+        raise ValueError
+    except ValueError:
+        print("Некорректный ввод.")
 
-mes = intcheck(input("Введите номер месяца: "))
+k = 0
 
-if mes in (1, 3, 5, 7, 8, 10, 12):
-    print("Дней в месяце - 31")
-elif mes in (4, 6, 9, 11):
-    print("Дней в месяце - 30")
-elif mes == 2:
-    print("Дней в месяце - 28")
+for i in range(0,1000000):
+    if 3**i < n:
+        k += 1
+    else:
+        break
+
+print("Наибольшее число K: ", k)
